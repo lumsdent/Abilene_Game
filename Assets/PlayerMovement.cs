ead42b4a9494da92562c10a300c6dbf9f1029b60
lumsdent <tyler.j.lumsden@gmail.com>
2023-01-17 22:05:32 -0500

    Added animations to player walk

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR.Haptics;
 using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
@@ -11,17 +12,27 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movement;
     Transform playerTransform;
     [SerializeField] float moveSpeed;
+    Animator animator;
+    string currentState;
     // Start is called before the first frame update
     void Awake()
     {
+        animator= GetComponent<Animator>();
         playerTransform = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.velocity = movement * moveSpeed;
+       
         StartCoroutine("CircleCheck");
     }
 
